5f523806e30e2e67858e6e79115618ce43594d99
Dohy2703 <125836071+Dohy2703@users.noreply.github.com>
2023-09-05 16:59:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Class
 from cam_operator import Operator
 from ultralytics import YOLO
 from realsense import Realsense
@@ -7,28 +6,28 @@ from TCPmanager import TCP
 from label_reader import labelReader
 
 from collections import defaultdict
-from collections import deque #status queue로 정의하기
+from collections import deque  # status queue로 정의하기
 import time
 import cv2
 import numpy as np
 import math as m
-
-
-realsense = True  # 지역변수 오류때문에 미리 선언해둠
-'''로봇의 state 정의'''
+import argparse
 
 
 # status = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# status = '5'
-status = '0'
-wait = False
-find_word = 623400
-query = None
-ip, port, tcp_available = '127.0.1.1', 2014, True
-book_detector = None
 
-if __name__ == '__main__':
-
+def librarian_main(
+        yolov8_weights=None,  # model.py path
+        source='/home/kdh/Downloads/20230801_002342.mp4',
+        realsense=True,  # realsense
+        status='0',
+        find_word=623400,
+        book_detector = None,
+        ip = '127.0.1.1',
+        port = 2014,
+        tcp_available = True,
+        query = None,
+):
     kdh = True
     ChadChang = False
     '''state마다 동작을 정해주는 클래스를 따로 만들음'''
@@ -43,16 +42,19 @@ if __name__ == '__main__':
         model = YOLO('/home/chad-chang/PycharmProjects/23_HF071/best.pt')
         book_detector = DetectBook(calb_path)
         realsense = True  # True면 뎁스카메라, False면 비디오q
-        tcp = TCP(ip, port, available= tcp_available)
+        tcp = TCP(ip, port, available=tcp_available)
+
+    # if yolov8_weights != None:
+    #     model = YOLO(yolov8_weights)
 
     # Flags
     tracking = False  # 글자 읽었을 때 트래킹
-    detect_center_book = True   # 말 그대로 중앙에 있는 책을 계속 검출할 것인지.
+    detect_center_book = True  # 말 그대로 중앙에 있는 책을 계속 검출할 것인지.
 
     if realsense:
         RS = Realsense()
     else:  # video
-        cap = cv2.VideoCapture('/home/kdh/Downloads/20230801_002342.mp4')
+        cap = cv2.VideoCapture(source)
 
     # reader instance
     book_detector.setup_reader()
@@ -68,7 +70,8 @@ if __name__ == '__main__':
     while True:
         starttime = time.time()
         if realsense:
-            results = model.track(RS.color_image, persist=True) # Run YOLOv8 tracking on the frame, persisting tracks between frames
+            results = model.track(RS.color_image,
+                                  persist=True)  # Run YOLOv8 tracking on the frame, persisting tracks between frames
             book_detector.get_image(RS)
         else:  # video
             _, frame = cap.read()
@@ -91,14 +94,13 @@ if __name__ == '__main__':
                     status = status_buff
                     # print('status = 0->1', status)
 
-
-                '''status1 : 책 읽으러 동작''' # 조금 수정할 필요있을듯
+                '''status1 : 책 읽으러 동작'''  # 조금 수정할 필요있을듯
             elif status == '1':  # 책 읽으러 동작
                 print("status1 = ", status)
                 if not wait:
                     # ret_r = detect_center_book.read_center_word(visualize = True)
-                    ret_r, H = runner.run_approach(book_detector, results, visual = True)
-                    print("H mat = ",H)
+                    ret_r, H = runner.run_approach(book_detector, results, visual=True)
+                    print("H mat = ", H)
                     # print(ret_r)
                     # print(runner.TF)
                     # 보내는 값이 온전한지 확인
@@ -119,8 +121,8 @@ if __name__ == '__main__':
             elif status == '2':  # readable한지 판단 & 방향 통신
                 print("status2 = ", status)
                 if not wait:
-                    ret, dir = runner.run_readable(book_detector, results, visual =True)
-                    if (not ret and dir != None): # 아무것도 안 잡힐때
+                    ret, dir = runner.run_readable(book_detector, results, visual=True)
+                    if (not ret and dir != None):  # 아무것도 안 잡힐때
                         print(dir)
                         tcp.send(dir)
                         wait = True
@@ -132,7 +134,7 @@ if __name__ == '__main__':
                 status_buff = tcp.receive()
                 # print("status2 =", status_buff)
                 if status_buff:
-                    print("buf = true",status_buff)
+                    print("buf = true", status_buff)
                     status = status_buff
                     wait = False
 
@@ -143,7 +145,7 @@ if __name__ == '__main__':
                     status = '3'
                     print("status_trans = ", 3)
                     tcp.send(3)  # 글씨 읽지 못한경우
-                else:  #글씨 읽기 실패했을 때
+                else:  # 글씨 읽기 실패했을 때
                     status = '8'
                     print("status_trans = ", 8)
                     # tcp.send(8)
@@ -166,13 +168,12 @@ if __name__ == '__main__':
                 #     print('word is f{}',find_word)
                 #     status = '3'
 
-                    # else: # 못 읽었으면 8번 상태로 가라 또는 다시 읽을까
-                    #     print('can not find the ')
-                    #     print('status = 2->8', status)
-                    #     tcp.send(8) # 글씨 읽지 못한경우
-                    #     status = '8'
-                    # print('status =', status)
-
+                # else: # 못 읽었으면 8번 상태로 가라 또는 다시 읽을까
+                #     print('can not find the ')
+                #     print('status = 2->8', status)
+                #     tcp.send(8) # 글씨 읽지 못한경우
+                #     status = '8'
+                # print('status =', status)
 
                 '''status3: 해당 책 위치로 이동'''
             elif status == '3':  # 해당책 위치로 이동:
@@ -187,7 +188,7 @@ if __name__ == '__main__':
                     if ret_r:  # 특징 추출 완료하면
                         wait = True
                         tcp.send(H, matrix=True)
-                    else: # 추출이 실패하면
+                    else:  # 추출이 실패하면
                         wait = False
 
                 status_buff = tcp.receive()
@@ -199,17 +200,19 @@ if __name__ == '__main__':
                     status = '5'
                     # 잠깐 쉬는 시간 가질까
 
-            elif status == '4': # 트래킹하면서 좌표 보내주기
+            elif status == '4':  # 트래킹하면서 좌표 보내주기
                 if results[0].boxes != None and results[0].boxes.id != None:
                     annotated_frame = results[0].plot()  # Visualize the results on the frame
                     if tracking:  # w 누르고 글자를 읽기 성공 시 트래킹
-                        book_detector.get_mask(results[0].boxes.cpu(), results[0].masks.data, results[0].names, tracking=tracking)  # 마스크 검출
+                        book_detector.get_mask(results[0].boxes.cpu(), results[0].masks.data, results[0].names,
+                                               tracking=tracking)  # 마스크 검출
                         label_id, book_id, label_idx, book_idx = book_detector.IDcheck(label_id, book_id)
                         if label_id == -1 and book_id == -1:
                             tracking = False
                             print('tracking = False')
                         else:
-                            book_detector.tracking_getter(label_idx, book_idx, visualize=True)  # 글자 읽은 책, 글자 읽은 라벨지, 책+라벨지 트래킹
+                            book_detector.tracking_getter(label_idx, book_idx,
+                                                          visualize=True)  # 글자 읽은 책, 글자 읽은 라벨지, 책+라벨지 트래킹
                             # 좌표 통신해 줌
                     cv2.imshow("YOLOv8 Tracking", annotated_frame)
                 else:  # not tracking
@@ -218,12 +221,12 @@ if __name__ == '__main__':
 
 
 
-            elif status == '5': # 최종적으로 넣기전에 그리퍼 align 맞추
+            elif status == '5':  # 최종적으로 넣기전에 그리퍼 align 맞추
                 print("status5 = ", status)
                 if not wait:
                     lc_point, blc_point = book_detector.LC_point, book_detector.BLC_point
                     # print(lc_point,blc_point)
-                    ang = book_detector.calc_ang(lc_point, blc_point,visual = True)
+                    ang = book_detector.calc_ang(lc_point, blc_point, visual=True)
                     print("angle = ", ang)
                     if ang:
                         tcp.send(ang)
@@ -240,20 +243,18 @@ if __name__ == '__main__':
                 print("status6 = ", status)
                 status_buff = tcp.receive()
                 if status_buff:
-                    print("buf = true",status_buff)
-                    status = status_buff # fail of suceed
+                    print("buf = true", status_buff)
+                    status = status_buff  # fail of suceed
 
             # elif status == '7':  # 그리핑 성공 여부 판단
             #     status_buff = tcp.receive
             #     if status_buff:
             #         status = 10
 
-
             # elif status == :
 
-
-            elif status == '8': # 해당 프레임에 책이 없을때 + 목표 책을 못 읽었을 경우도 있음(책 인식 모델이 매우 정확도가 높은 것이기 때문에 고려하지 않기)
-                tcp.send(1) # not read symbol
+            elif status == '8':  # 해당 프레임에 책이 없을때 + 목표 책을 못 읽었을 경우도 있음(책 인식 모델이 매우 정확도가 높은 것이기 때문에 고려하지 않기)
+                tcp.send(1)  # not read symbol
                 print("status8 = ", status)
                 status_buff = tcp.receive()
                 if status_buff:
@@ -273,7 +274,7 @@ if __name__ == '__main__':
                 #     wait = False
 
 
-            elif status == 'f': # 로봇팔 구동이 끝나고 유석이 쪽으로 신호줄때
+            elif status == 'f':  # 로봇팔 구동이 끝나고 유석이 쪽으로 신호줄때
                 status_buff = tcp.receive()
 
             # elif status == 's':
@@ -281,7 +282,8 @@ if __name__ == '__main__':
         if results[0].boxes != None and results[0].boxes.id != None:
             annotated_frame = results[0].plot()  # Visualize the results on the frame
             if tracking:  # w 누르고 글자를 읽기 성공 시 트래킹
-                book_detector.get_mask(results[0].boxes.cpu(), results[0].masks.data, results[0].names, tracking=tracking)  # 마스크 검출
+                book_detector.get_mask(results[0].boxes.cpu(), results[0].masks.data, results[0].names,
+                                       tracking=tracking)  # 마스크 검출
                 label_id, book_id, label_idx, book_idx = book_detector.IDcheck(label_id, book_id)
                 if label_id == -1 and book_id == -1:
                     tracking = False
@@ -291,8 +293,6 @@ if __name__ == '__main__':
             cv2.imshow("YOLOv8 Tracking", annotated_frame)
         else:  # not tracking
             cv2.imshow("YOLOv8 Tracking", results[0].plot())
-
-
 
         endtime = time.time()
         # print('처리시 간= ',endtime-starttime)
@@ -317,7 +317,7 @@ if __name__ == '__main__':
         elif k_ == ord('e'):
             if realsense:
                 book_detector.get_image(RS)
-            else :
+            else:
                 book_detector.get_video_image(frame)
             book_detector.get_mask(results[0].boxes.cpu(), results[0].masks.data, results[0].names, tracking=tracking)
             book_detector.center_book(visualize=True)
@@ -325,3 +325,23 @@ if __name__ == '__main__':
             # ret_r, H = runner.run_approach(book_detector, results, visual = True)
             cv2.waitKey(0)
 
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='./best_v8.pt', help='yolov8 model path(s)')
+    parser.add_argument('--source', type=str, default=None, help='source other than realsense')
+    parser.add_argument('--realsense', type=bool, default=True, help='if you use realsense depth cam')
+    parser.add_argument('--status', type=str, default='0', help='initial robot arm status')
+    parser.add_argument('--find-word', type=int, default=623400, help='book label number to find')
+    parser.add_argument('--book-detector', default=None, help='detectBook instance')
+    parser.add_argument('--ip', type=str, default='127.0.1.1', help='tcp ip communication')
+    parser.add_argument('--port', type=int, default=2014, help='tcp ip port')
+    parser.add_argument('--tcp-available',type=bool, default=True, help='tcp available')
+    parser.add_argument('--query', default=None, help=' ')
+    opt = parser.parse_args()
+    return opt
+
+
+if __name__ == '__main__':
+    opt = parse_opt()
+    librarian_main(opt)
